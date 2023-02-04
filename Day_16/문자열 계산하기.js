4ae321e7c7dab64dc399256c65f899ceb9f52c9b
0uizi0 <0uizi0@naver.com>
2023-02-04 17:43:53 +0900

    #문자열 계산하기

---
+++ Day_16/문자열 계산하기.js
@@ -0,0 +1,12 @@
+const solution = (my_string) => {
+  const str = my_string.split(" ");
+  let result = Number(str[0]);
+  for (let i = 1; i <= str.length; i += 2) {
+    if (str[i] === "+") {
+      result += Number(str[i + 1]);
+    } else if (str[i] === "-") {
+      result -= Number(str[i + 1]);
+    }
+  }
+  return result;
+};
