2d609b8004699aec07c6066dacedca6854037d02
0uizi0 <0uizi0@naver.com>
2023-02-07 22:54:22 +0900

    #잘라서 배열로 저장하기

---
+++ Day_19/잘라서 배열로 저장하기.js
@@ -0,0 +1,8 @@
+const solution = (my_str, n) => {
+  const str = my_str.split("");
+  const result = [];
+  while (str.length > 0) {
+    result.push(str.splice(0, n).join(""));
+  }
+  return result;
+};
