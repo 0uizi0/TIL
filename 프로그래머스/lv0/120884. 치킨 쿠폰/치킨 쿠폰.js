6bd33ec7970b4dedf5761842c3b8720162a2022c
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-05-04 11:35:58 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,8 @@
+const solution = (chicken) => {
+    let result = 0
+    while (chicken >= 10) {
+        result += Math.floor(chicken/10)
+        chicken = Math.floor(chicken/10) + chicken % 10
+    }
+    return result
+}
