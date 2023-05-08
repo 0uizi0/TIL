8e3b8e9049e76755285cd55ce731c65d910a0287
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-05-08 17:34:03 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,9 @@
+const solution = (my_string) => {
+    if (my_string.match(/\d+/g) == null) {
+        return 0;
+    } else {
+        let result = 0;
+        for (let i of my_string.match(/\d+/g)) { result += Number(i); }
+        return result;
+    }
+}
