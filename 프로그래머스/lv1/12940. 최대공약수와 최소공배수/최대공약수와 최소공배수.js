9873023fd2501772d636a9c755388930a9b7c26e
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-05-16 13:50:38 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,6 @@
+function solution(n, m) {
+    // 유클리드 호제법 : GCD(n, m) == GCD(m, n % m)
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    const lcm = (a, b) => a * b / gcd(a, b);
+    return [gcd(n, m), lcm(n, m)];
+}
