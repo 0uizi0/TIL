28ba9012bd76da09cf6c129f8a7b75f4804499b8
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-05-22 15:45:37 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,5 @@
+const solution = (num_list) => {
+    const multi = num_list.reduce((acc,cur) => acc *= cur)
+    const sum = num_list.reduce((acc,cur) => acc += cur)
+    return multi < sum**2 ? 1 : 0
+}
