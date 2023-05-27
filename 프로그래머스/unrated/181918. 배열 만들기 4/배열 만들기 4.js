b8df6674c32261cd9329bd2ae0974132bde94d3c
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-05-27 13:21:35 +0900

    [unrated] Title: 배열 만들기 4, Time: 26.79 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,17 @@
+const solution = (arr) => {
+    let stk = [], i = 0;
+    while (i < arr.length) {
+        if (stk.length == 0) {
+            stk.push(arr[i])
+            i++
+        }
+        else {
+            if (stk[stk.length-1] >= arr[i] ) { stk.pop() } 
+            else {
+                stk.push(arr[i])
+                i++
+            }
+        }
+    }
+    return stk;
+}
