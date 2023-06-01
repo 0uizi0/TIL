5e4c25551171200c1ebf66345353b6ae637a594a
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-06-01 10:17:54 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,7 @@
+const solution = (my_string, is_prefix) => {
+    const prefix = []
+    for (let i = 0; i < my_string.length; i++) {
+        prefix.push(my_string.slice(0,i+1))
+    }
+    return prefix.includes(is_prefix) ? 1 : 0
+}
