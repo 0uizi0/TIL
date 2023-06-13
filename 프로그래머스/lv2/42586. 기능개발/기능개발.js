d5cd7a8c007f96fb56501cb10c4edd880cd03963
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-06-13 17:13:20 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,11 +1,22 @@
 const solution = (progresses, speeds) =>  {
+    const result = []
+    
     const workTime = progresses.map((v,i) => Math.ceil((100 - v) / speeds[i]));
-    const result = workTime.map((v,i) => {
-        if (v > workTime[i + 1]) workTime[i + 1] = v
-        return v
-    }).reduce((acc,cur) => {
-        acc[cur] = (acc[cur] || 0) + 1;
-        return acc
-    },{})
-    return Object.values(result);
+    
+    let cnt = 1;
+    let date = workTime[0]
+    
+    for (let i = 1; i < workTime.length; i++) {
+        
+        if (workTime[i] <= date) {
+            cnt++
+        } else {
+            date = workTime[i]
+            result.push(cnt);
+            cnt = 1;
+        }
+    }
+    result.push(cnt);
+    
+    return result;
 }
