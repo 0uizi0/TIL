2153aedb463ba986c756515515246229d3089611
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-06-13 13:45:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.73 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,3 @@
 const solution = (arr) => {
-    let answer = [];
-    for (let i=0; i<arr.length; i++) {
-        if (arr[i] !== arr[i+1]) answer.push(arr[i])
-    }
-    return answer
+    return arr.filter((_,i) => arr[i] !== arr[i+1])
 }
