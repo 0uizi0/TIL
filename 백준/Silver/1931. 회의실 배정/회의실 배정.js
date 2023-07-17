c2c69f39d8c8e1e0ae75720c28f500b49a393547
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-07-18 02:09:10 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 43332 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const filePath = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let count = Number(input.shift());
+const count = Number(input.shift());
 let list = [];
 for (let i of input) {
-  list.push(i.split(" ").map((n) => Number(n)));
+  list.push(i.split(" ").map(Number));
 }
 
 list.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
