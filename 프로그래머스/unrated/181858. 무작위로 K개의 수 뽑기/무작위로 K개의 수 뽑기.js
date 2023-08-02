215a7dc9c0388f705a6246141d001437a1886ec5
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2023-08-02 22:36:50 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 23.55 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,8 @@
+function solution(arr, k) {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i]) && result.length < k) result.push(arr[i])
+    }
+    while(result.length < k) result.push(-1) 
+    return result
+}
