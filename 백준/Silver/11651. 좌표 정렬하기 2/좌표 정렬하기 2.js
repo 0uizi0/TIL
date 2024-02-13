fa19809cf6df37520e30132fd8289d53364165e2
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2024-02-13 11:03:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 492 ms, Memory: 59048 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = Number(input.shift().split(" "));
+const positions = input.map((v) => v.split(" ").map(Number));
+const result = [];
+
+// 오름차순 정렬
+const sortedArr = positions.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  else return a[1] - b[1];
+});
+
+for (let i = 0; i < N; i++) {
+  const r = sortedArr[i].join(" ");
+  result.push(r);
+}
+
+// 출력
+console.log(result.join("\n"));
