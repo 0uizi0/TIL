32a4903bafac2be21271bf198a5984fa37a5992d
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2024-02-16 10:21:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 18212 KB -BaekjoonHub

---
+++ 백준/Bronze II/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+if (input.length === 1 && input[0] === "") console.log(0);
+else console.log(input.length);
