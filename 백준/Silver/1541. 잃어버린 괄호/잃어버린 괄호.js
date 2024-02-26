3b0fee1f7b298a78228f91b66c7ae304aa22f55b
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2024-02-26 10:09:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,19 +1,27 @@
-// input
 const fs = require("fs");
 const filePath = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+let result = 0;
+let count = 0;
 
-// GREEDY
-const firstOrder = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("-")
-  .map((formula) => {
-    let num = formula.split("+");
-    return num.reduce((n1, n2) => Number(n1) + Number(n2), 0);
-  });
+/** 입력값이 첫 번째 숫자인지 확인 후, true라면 더하고 false면 빼주기 */
+const calNum = (num) => {
+  if (count === 0) result += Number(num);
+  else result -= Number(num);
 
-const nextOrder = firstOrder.reduce((acc, cur) => (acc -= cur));
+  count++;
+};
 
-// output
-console.log(nextOrder);
+// - 연산자를 기준으로 문자열 분리
+input.split("-").map((v) => {
+  if (!v.includes("+")) return calNum(v);
+
+  // + 연산자를 기준으로 문자열 분리
+  const splitPlus = v
+    .split("+")
+    .map(Number)
+    .reduce((acc, cur) => (acc += Number(cur)), 0);
+  return calNum(splitPlus);
+});
+
+console.log(result);
