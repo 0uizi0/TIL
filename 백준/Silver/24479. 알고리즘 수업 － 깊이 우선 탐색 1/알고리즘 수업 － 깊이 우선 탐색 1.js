2efe51c9992458ab6cc8f78342770c19665e846a
Eun-jeong Kim <102431281+0uizi0@users.noreply.github.com>
2024-10-31 21:56:00 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 712 ms, Memory: 118508 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -0,0 +1,37 @@
+// input
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [[N, M, R], ...edges] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+// G (graph)
+const G = Array.from(new Array(N + 1), () => []);
+
+edges.map(([u, v]) => {
+  G[u].push(v);
+  G[v].push(u);
+});
+
+G.map((node) => node.sort((a, b) => a - b));
+
+// DFS
+const visited = Array(N + 1).fill(0);
+let visitOrder = 1;
+
+function dfs(node) {
+  if (!visited[node]) {
+    visited[node] = visitOrder;
+    visitOrder++;
+
+    G[node].forEach((next) => dfs(next));
+  }
+}
+
+dfs(R);
+
+// output
+console.log(visited.slice(1).join("\n"));
